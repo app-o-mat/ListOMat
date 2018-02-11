4ac439ed23dd06fb73a091de152b2c0eaae6fb08
Lou Franco <lfranco@greenwave-solutions.com>
2018-02-11 09:16:57 -0500

    Initial commit - mostly the master/detail template

---
+++ ListOMat/ListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ListViewController.swift
+//  ListOMat
+//
+//  Created by Louis Franco on 2/11/18.
+//  Copyright © 2018 App-o-Mat. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
